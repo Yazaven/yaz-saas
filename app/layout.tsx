d1b8fc377342d244e7d7120fac6ee3947dfbb843
Yazaven <mwassiyazan@protonmail.com>
2025-07-15 22:17:23 +0300

    bug fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default async function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="Light"
           enableSystem
           disableTransitionOnChange
         >
